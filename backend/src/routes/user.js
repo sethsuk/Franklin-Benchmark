a9279867fd216d2cdfe229323962ec49b9783131
Seth Sukboontip <sukboontips@gmail.com>
2025-04-13 12:13:44 -0400

    created account_age endpoint

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -11,7 +11,7 @@ const client = new OAuth2Client(client_id);
 
 // POST endpoint => authenticate the user
 router.post('/auth/google', async (req, res) => {
-    console.log('\n\Auth Google Called');
+    console.log('\n\nAuth Google Called');
 
     const { idToken } = req.body;
 
@@ -46,11 +46,11 @@ router.post('/auth/google', async (req, res) => {
                 { expiresIn: '1h' }
             );
 
-            res.json({ status: 'new_user', googleId, email, token });
+            res.json({ status: 'new_user', user: { googleId, email, username: null, id: null }, token });
         }
 
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(401).json({ message: 'Invalid Google token' });
     }
 });
@@ -92,9 +92,9 @@ router.post('/auth/register-username', async (req, res) => {
             { expiresIn: '7d' }
         );
       
-        res.status(201).json({ message: 'User created successfully', username, token });
+        res.status(201).json({ message: 'User created successfully', user: newUser, token });
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(500).json({ message: 'Database error' });
     }
 });
@@ -110,14 +110,41 @@ const authenticateToken = (req, res, next) => {
     // verify token
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
+
         req.user = user; // userId and username will be available
         next();
     });
 };
 
+// GET endpoint => checks if the user is valid and verified
 router.get('/verify', authenticateToken, (req, res) => {
     res.json({ message: 'User verified', user: req.user });
 });
 
+// GET endpoint => returns how old the account is in days
+router.get('/account-age', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        // queries DB to find the user's account age
+        const result = await pool.query(`
+            SELECT (CURRENT_DATE - created_at::date) AS account_age
+            FROM users
+            WHERE id = $1
+        `, [userId]);
+
+        // No result from DB
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+
+        const { account_age } = result.rows[0];
+
+        return res.status(200).json({ account_age});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
 
 module.exports = router;
